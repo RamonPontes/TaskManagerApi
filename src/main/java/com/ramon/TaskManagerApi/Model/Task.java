cb9e59fbb378e3e0e11cf957591899f4aeaec95c
RamonPontes <rgdpontes@yahoo.com>
2024-12-05 19:58:44 -0300

    Add task model

--- src/main/java/com/ramon/TaskManagerApi/Model/Task.java
+++ src/main/java/com/ramon/TaskManagerApi/Model/Task.java
@@ -1,4 +1,62 @@
 package com.ramon.TaskManagerApi.Model;
 
+import java.time.LocalDateTime;
+
 public class Task {
+
+    private int id;
+    private String title;
+    private String description;
+    private String createdBy;
+    private String status;
+    private LocalDateTime createdAt;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getCreatedBy() {
+        return createdBy;
+    }
+
+    public void setCreatedBy(String createdBy) {
+        this.createdBy = createdBy;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
 }
