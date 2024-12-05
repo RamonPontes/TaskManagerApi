56e49830de3c5483f841c24bcd8fbf2e26fc795c
RamonPontes <rgdpontes@yahoo.com>
2024-12-05 19:54:32 -0300

    Add all endpoints

--- src/main/java/com/ramon/TaskManagerApi/Controller/TaskController.java
+++ src/main/java/com/ramon/TaskManagerApi/Controller/TaskController.java
@@ -1,4 +1,36 @@
 package com.ramon.TaskManagerApi.Controller;
 
+import com.ramon.TaskManagerApi.Model.Task;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/tasks")
 public class TaskController {
+
+    @GetMapping
+    public ResponseEntity<String> getAllTasks() {
+        return ResponseEntity.status(200).body("Ok");
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<String> getTaskById() {
+        return ResponseEntity.status(200).body("Ok");
+    }
+
+    @PostMapping
+    public ResponseEntity<String> createdTask() {
+        return ResponseEntity.status(200).body("Ok");
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<String> editTask() {
+        return ResponseEntity.status(200).body("Ok");
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteTask() {
+        return ResponseEntity.status(200).body("Ok");
+    }
+
 }
